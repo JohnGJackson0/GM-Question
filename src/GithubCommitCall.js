ea8c9a1e6e97a00b8e9a48ab46a49c73797c47b2
jgjio <johngjackson0@gmail.com>
2021-09-06 19:35:03 -0400

    Implement github call

--- src/GithubCommitCall.js
+++ src/GithubCommitCall.js
@@ -2,7 +2,16 @@ import React from "react";
 import { Octokit } from "@octokit/core";
 
 export async function GithubCommitCall() {
+  const octokit = new Octokit({
+    auth: "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",
+  });
 
+  const commits = await octokit.request("GET /search/commits", {
+    q: "q",
+    mediaType: {
+      previews: ["cloak"],
+    },
+  });
 
   return commits;
 }
