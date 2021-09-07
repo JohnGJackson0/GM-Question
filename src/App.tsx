83fad1ad718a443cd089122332a68194c7600cbf
jgjio <johngjackson0@gmail.com>
2021-09-07 07:15:56 -0400

    Show github commits

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { registerRootComponent } from "expo";
+import { GithubCommits } from "./GithubCommits";
 
 function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <GithubCommits />
       <StatusBar style="auto" />
     </View>
   );
