aea78ac57eba54b3902486775f2959e9206d2f3c
jgjio <johngjackson0@gmail.com>
2021-09-07 07:15:32 -0400

    Create test for github messages

--- src/__tests/GithubCommits.test.tsx
+++ src/__tests/GithubCommits.test.tsx
@@ -3,8 +3,16 @@ import { render } from "@testing-library/react-native";
 import { GithubCommits } from "../GithubCommits";
 
 describe("GithubCommits", () => {
+  beforeEach(() => {
+    jest.mock("../__mocks__/octokit");
+  });
+
   test("Should render correctly", () => {
     const component = render(<GithubCommits />);
     component.getByTestId("github-commits");
+  });
+  test("Should render Github message", async () => {
+    const { getByTestId } = render(<GithubCommits />);
+    await getByTestId("commit-item-0");
   });
 });
