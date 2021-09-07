b775d446c277b68d69bcc606bdd1eea726a2eacd
jgjio <johngjackson0@gmail.com>
2021-09-07 06:47:09 -0400

    Display github commit

--- src/GithubCommits.tsx
+++ src/GithubCommits.tsx
@@ -1,6 +1,30 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { GithubCommit } from "./GithubCommit";
+import { GithubCommitCall } from "./GithubCommitCall";
 
 export const GithubCommits = () => {
-  return <View testID="github-commits"></View>;
+  const [apiResponse, setApiResponse] = useState<any>();
+
+  useEffect(() => {
+    GithubCommitCall().then((response) => {
+      setApiResponse(response);
+    });
+  }, []);
+
+  return (
+    <View testID="github-commits">
+      {apiResponse == undefined ? (
+        <></>
+      ) : (
+        <View>
+          <GithubCommit
+            message={apiResponse.data.items[0].commit.message}
+            author={apiResponse.data.items[0].commit.author.name}
+            hash={apiResponse.data.items[0].sha}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
